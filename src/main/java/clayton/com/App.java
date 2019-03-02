6ce014af6e739b2fa51a64a499f1ed24ef325789
Clayton <cmpetersen@hotmail.co.za>
2019-03-02 15:56:52 +0200

    Initial commit

---
+++ src/main/java/clayton/com/App.java
@@ -0,0 +1,22 @@
+package clayton.com;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    static Scanner input = new Scanner(System.in);
+
+    public static void main( String[] args ){
+
+        System.out.print( "What 3rd year stream are you in?: " );
+
+        String ans = input.next();
+
+        //add some more code
+
+    }
+}
